9b2b86888aebfebdc2d0484798d1287275e1329c
A2700 <128379068+A2700@users.noreply.github.com>
2024-09-28 20:56:30 +0330

    Add files via upload

---
+++ beta/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
