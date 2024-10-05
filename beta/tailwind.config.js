7b02576ca39cd348c814d39361bc1a882389747e
A2700 <128379068+A2700@users.noreply.github.com>
2024-10-05 16:32:37 +0330

    Update tailwind.config.js

--- beta/tailwind.config.js
+++ beta/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': {'min': '320px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    },
   },
   plugins: [],
 }
